db2a9ed80e96115b610547c307cd41d9defd8d06
Akash Trehan <akash.trehan123@gmail.com>
2016-10-12 03:08:05 +0530

    Trying to fix server error 3

--- eventual/urls.py
+++ eventual/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from website import urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include(website.urls, namespace='website')),
+    url(r'^', include(urls, namespace='website')),
 ]
